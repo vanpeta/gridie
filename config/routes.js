5f12ee91c4e26a85ca396ded7f9220fecf677f56
Guess/carlosallende <callende@guess.com>
2018-02-28 10:22:05 -0800

    adding first view

--- config/routes.js
+++ config/routes.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-app.get("/", (req, res) => {
-  res.sendfile('public/index.html');
+router.get("/", (req, res) => {
+  res.sendfile("public/index.html");
 });
 
 module.exports = router;
