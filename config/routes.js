668bfb3d1ddfc6c0d80062fb3156b4db9187dae3
Guess/carlosallende <callende@guess.com>
2018-02-28 13:50:29 -0800

    react added to front end, proxy added to package json. nodemon, concurrently and logic for production

--- config/routes.js
+++ config/routes.js
@@ -1,8 +1,5 @@
-const express = require('express');
-const router = express.Router();
-
-router.get("/", (req, res) => {
-  res.sendfile("public/index.html");
-});
-
-module.exports = router;
+module.exports = app => {
+	app.get("/", (req, res) => {
+    res.sendFile("public/index.html");
+  });
+}
