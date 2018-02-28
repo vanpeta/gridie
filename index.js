a585918f54be4bab320f88782f05d344d48138ad
Guess/carlosallende <callende@guess.com>
2018-02-28 10:19:41 -0800

    adding routes

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const app = express();
+const routes = require("./config/routes");
 
-app.get('/', (req, res) => {
-	res.send({hi: 'buddie'});
-});
+app.use("/", routes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
