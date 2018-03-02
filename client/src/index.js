40a8e34d409df3bb44274d2c9b277d1ca6d4ff0a
Guess/carlosallende <callende@guess.com>
2018-03-02 13:57:06 -0800

    createing react app again

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,8 @@
-import "materialize-css/dist/css/materialize.min.css";
-import React from "react";
-import ReactDom from "react-dom";
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
-import reduxThunk from "redux-thunk";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-import App from "./components/App";
-import reducers from "./reducers/";
-
-const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
-
-ReactDom.render(
-  <Provider store={store}><App /></Provider>,
-  document.querySelector('#root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
--- client/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import { reducer as reduxForm } from "redux-form";
-import categoryReducer from './categoryReducer';
-
-export default combineReducers({
-  category: categoryReducer,
-  form: reduxForm
-});
