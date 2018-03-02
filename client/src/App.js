352302eb3f522d5ff070135dc2cce01331d633cd
Guess/carlosallende <callende@guess.com>
2018-03-02 14:54:59 -0800

    new client, axios in the backend too

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,46 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { url: "" };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  onInputChange(term) {
+    this.setState({ url: term });
+  }
+
+  handleClick() {
+    const config = {
+      method: "get",
+      url: 'api/get_products',
+      params: this.state.url + "?json=1",
+      responseType: "json",
+      crossdomain: true,
+      withCredentials: true
+    };
+    axios.request(config).then(response => this.handleProducts(response));
+  }
+
+  handleProducts(response) {
+    const products = response.data.Value.Abstract.Products;
+    console.log(products);
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <header className="App-header" />
+        <label htmlFor="url">Category url</label>
+        <input
+          type="text"
+          id="url"
+          value={this.state.url}
+          onChange={event => this.onInputChange(event.target.value)}
+        />
+        <button onClick={this.handleClick}>Get Gridie</button>
       </div>
     );
   }
--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,20 @@
+const axios = require("axios");
+
 module.exports = app => {
 	app.get("/api/get_products", (req, res) => {
-    console.log(req.body);
-    res.send({greeting: "HOLA"});
+    const url = req.query[0];
+    if (!url) {
+      res.status(400);
+      res.send('bad request: please provide a url');
+    } else {
+      axios
+        .get(url)
+        .then(response => {
+          res.send(response.data);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
   });
-}
+};
