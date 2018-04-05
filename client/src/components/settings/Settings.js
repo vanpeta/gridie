a3a20d8fd89888c3a877f229184e93c6f331211b
Guess/carlosallende <callende@guess.com>
2018-04-05 14:27:36 -0700

    refactoring settings

--- client/src/components/settings/Settings.js
+++ client/src/components/settings/Settings.js
@@ -1,46 +1,15 @@
 import React, { Component } from "react";
-import { connect } from "react-redux";
-import { bindActionCreators } from "redux";
 
-import { numberOfProducts } from "../../actions/index";
 import InputUrl from "./InputUrl"
 import InputTitle from "./InputTitle";
+import InputNumberOrProducts from "./InputNumberOfProducts";
 
 class Settings extends Component {
-	constructor(props) {
-		super(props);
-		this.handleChange = this.handleChange.bind(this);
-	}
-	handleChange(e) {
-		this.props.numberOfProducts(e.target.value)
-	}
   render() {
     return (
       <div className="container">
 				<div className="row">
-					<div className="form-group">
-						<label htmlFor="productsInput">
-							Number of products:
-						</label>
-						<select 
-							className="form-control form-control-sm"
-							id="productsInput"
-							type="number"
-							min="1"
-							max="9"
-							step="1"
-							onChange={this.handleChange}>
-								<option>1</option>
-								<option>2</option>
-								<option>3</option>
-								<option>4</option>
-								<option>5</option>
-								<option>6</option>
-								<option>7</option>
-								<option>8</option>
-								<option>9</option>
-						</select>
-					</div>
+					<InputNumberOrProducts />
 				</div>
 				<div className="row">
 					<InputUrl />
@@ -53,13 +22,6 @@ class Settings extends Component {
   }
 }
 
-function mapDispatchToProps(dispatch) {
-  return bindActionCreators(
-    {
-      numberOfProducts
-    },
-    dispatch
-  );
-}
 
-export default connect(null, mapDispatchToProps)(Settings);
+
+export default Settings;
