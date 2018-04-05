608d448e521aea4fcf80186c48b3ffc04017d49e
Guess/carlosallende <callende@guess.com>
2018-04-05 10:52:16 -0700

    Adding resposiveness

--- client/src/components/settings/snippets/Product.js
+++ client/src/components/settings/snippets/Product.js
@@ -7,23 +7,21 @@ const Product = props => {
 				<tr>
 					<td align="center" width="100%" style="padding-top: 20px; padding-bottom:20px; padding-left:5%; padding-right:5%; font-family: CenturyGothic, Futura, Verdana, sans-serif; font-size: 20px; color: #000000; letter-spacing: .15em; line-height: 200%;">
 						<a _label="product-grid" href="${props.data.link}">
-							<img width="100%" src="${
-                props.data.image
-              }" style="display:block; border-style: none;">
+							<img width="100%" src="${props.data.image}" style="display:block; border-style: none;">
 						</a>
 					</td>
 				</tr>
 				<tr>
 					<td align="center" style="padding-left:5%; padding-right:5%;">
 						<a _label="product-grid" href="${props.data.link}" class="gridName" style="text-decoration: none; font-family: 'Century Gothic', Futura, Verdana, sans-serif; font-size: 13px; color: #000000; letter-spacing: .15em; line-height: 200%; font-weight: bold;">${
-						props.data.name
+							props.data.name
 						}</a>
 					</td>
 				</tr>
 				<tr>
 					<td align="center" style="padding-left:5%; padding-right:5%;">
 						<a _label="product-grid" href="${props.data.link}" class="gridName" style="text-decoration: none; font-family: 'Century Gothic', Futura, Verdana, sans-serif; font-size: 13px; color: #000000; letter-spacing: .15em; line-height: 200%; font-weight: bold;">${
-						props.data.price
+							props.data.price
 						}</a>
 					</td>
 				<tr>
--- client/src/components/settings/snippets/Snippet.js
+++ client/src/components/settings/snippets/Snippet.js
@@ -1,6 +1,37 @@
 import React, { Component } from "react";
 import Product from "./Product";
 
+const style = `<!--[if !mso]><!-- -->
+<style type="text/css">
+ @media only screen and (max-width: 480px) {
+  .MobileCtaTextSize {
+   font-size: 2.5vw !important;
+  }
+ }
+
+ @media only screen and (max-width: 600px) {
+  .hide_mobile {
+   display: none !important;
+   width: 0px !important;
+   height: 0px !important;
+   visibility: hidden !important;
+   overflow: hidden !important;
+   font-size: 0px !important;
+   line-height: 0px !important;
+  }
+  .show_for_mobile {
+   display: block !important;
+   width: auto !important;
+   height: auto !important;
+   overflow: visible !important;
+   position: static !important;
+   min-height: 0px !important;
+   line-height: normal !important;
+   font-size: medium !important;
+  }
+ }
+</style>
+<!--<![endif]-->`;
 
 class Snippet extends Component {
   render () {
@@ -30,7 +61,8 @@ class Snippet extends Component {
     case "3":
       return (
         <div id="snippet">
-        {`<table align="center" width="100%" border="0" cellpadding="0" cellspacing="0">
+          {style +
+        `<table align="center" width="100%" border="0" cellpadding="0" cellspacing="0" class="hide_mobile">
             <tr>
               <td align="center" width="33.3%">`}
                 <Product data={this.props.data[0]} />
@@ -42,7 +74,29 @@ class Snippet extends Component {
                 <Product data={this.props.data[2]} />
             {`</td>
             </tr>
-          </table>`}
+          </table>
+          <!-- mobile view -->
+          <!--[if !mso]><!-- -->
+          <div class="show_for_mobile" align="center" style="display: none; width: 0px; height: 0px; overflow: hidden; min-height: 0px; line-height: 0px; font-size: 0px;">
+            <table align="center" width="100%" border="0" cellpadding="0" cellspacing="0">
+              <tr>
+                <td align="center" width="50%">`}
+                  <Product data={this.props.data[0]} />
+              {`</td>
+                <td align="center" width="50%">`}
+                  <Product data={this.props.data[1]} />
+              {`</td>
+              </tr>
+            </table>
+            <table align="center" width="100%" border="0" cellpadding="0" cellspacing="0">
+              <tr>
+                <td align="center" width="50%" style="max-width: 50%;">`}
+                  <Product data={this.props.data[2]} />
+              {`</td>
+              </tr>
+            </table>
+          </div>
+          <!--<![endif]-->`}
         </div>
       );
     case "4":
@@ -73,35 +127,70 @@ class Snippet extends Component {
     case "5":
       return (
         <div id="snippet">
-        {`<table align="center" width="100%" border="0" cellpadding="0" cellspacing="0">
+          {style +
+          `<table align="center" width="100%" border="0" cellpadding="0" cellspacing="0" class="hide_mobile">
             <tr>
               <td align="center" width="33.3%">`}
                 <Product data={this.props.data[0]} />
             {`</td>
               <td align="center" width="33.3%">`}
                 <Product data={this.props.data[1]} />
-            {`</td>
-              <td align="center" width="33.3%">`}
+              {`</td>
+                <td align="center" width="33.3%">`}
                 <Product data={this.props.data[2]} />
               {`</td>
-            </tr>
-          </table>
-          <table width="100%" align="center" cellspacing="0" cellpadding="0">
-            <tr>
-              <td width="50%" align="center">`}
-                <Product data={this.props.data[3]} />
-            {`</td>
-              <td width="50%" align="center">`}
-                <Product data={this.props.data[4]} />
-            {`</td>
-            </tr>
-          </table>`}
-        </div>
-      );
+              </tr>
+            </table>
+            <table width="100%" align="center" cellspacing="0" cellpadding="0" class="hide_mobile">
+              <tr>
+                <td width="50%" align="center">`}
+                  <Product data={this.props.data[3]} />
+              {`</td>
+                <td width="50%" align="center">`}
+                  <Product data={this.props.data[4]} />
+              {`</td>
+              </tr>
+            </table>
+            <!-- mobile view -->
+            <!--[if !mso]><!-- -->
+            <div class="show_for_mobile" align="center" style="display: none; width: 0px; height: 0px; overflow: hidden; min-height: 0px; line-height: 0px; font-size: 0px;">
+              <table align="center" width="100%" border="0" cellpadding="0" cellspacing="0">
+                <tr>
+                  <td align="center" width="50%">`}
+                    <Product data={this.props.data[0]} />
+                {`</td>
+                  <td align="center" width="50%">`}
+                    <Product data={this.props.data[1]} />
+                {`</td>
+                </tr>
+              </table>
+              <table align="center" width="100%" border="0" cellpadding="0" cellspacing="0">
+                <tr>
+                  <td align="center" width="50%">`}
+                    <Product data={this.props.data[2]} />
+                {`</td>
+                  <td align="center" width="50%">`}
+                    <Product data={this.props.data[3]} />
+                {`</td>
+                </tr>
+              </table>
+              <table align="center" width="100%" border="0" cellpadding="0" cellspacing="0">
+                <tr>
+                  <td align="center" width="50%" style="max-width: 50%;">`}
+                    <Product data={this.props.data[4]} />
+                {`</td>
+                </tr>
+              </table>
+            </div>
+            <!--<![endif]-->
+            `}
+          </div>
+        );
     case "6":
       return (
         	<div id="snippet">
-      		{`<table align="center" width="100%" border="0" cellpadding="0" cellspacing="0">
+          {style +
+      		`<table align="center" width="100%" border="0" cellpadding="0" cellspacing="0" class="hide_mobile">
 			      	<tr>
 					      <td align="center" width="33.3%">`}
       			      <Product data={this.props.data[0]} />
@@ -114,7 +203,7 @@ class Snippet extends Component {
                 {`</td>
               </tr>
             </table>
-			      <table align="center" width="100%" border="0" cellpadding="0" cellspacing="0">
+			      <table align="center" width="100%" border="0" cellpadding="0" cellspacing="0" class="hide_mobile">
 				      <tr>
 					      <td align="center" width="33.3%">`}
       			      <Product data={this.props.data[4]} />
@@ -126,13 +215,49 @@ class Snippet extends Component {
                   <Product data={this.props.data[6]} />
                 {`</td>
               </tr>
-            </table>`}
+            </table>
+            <!-- mobile view -->
+            <!--[if !mso]><!-- -->
+            <div class="show_for_mobile" align="center" style="display: none; width: 0px; height: 0px; overflow: hidden; min-height: 0px; line-height: 0px; font-size: 0px;">
+              <table align="center" width="100%" border="0" cellpadding="0" cellspacing="0">
+                <tr>
+                  <td align="center" width="50%">`}
+                    <Product data={this.props.data[0]} />
+                {`</td>
+                  <td align="center" width="50%">`}
+                    <Product data={this.props.data[1]} />
+                {`</td>
+                </tr>
+              </table>
+              <table align="center" width="100%" border="0" cellpadding="0" cellspacing="0">
+                <tr>
+                  <td align="center" width="50%">`}
+                    <Product data={this.props.data[2]} />
+                {`</td>
+                  <td align="center" width="50%">`}
+                    <Product data={this.props.data[3]} />
+                {`</td>
+                </tr>
+              </table>
+              <table align="center" width="100%" border="0" cellpadding="0" cellspacing="0">
+                <tr>
+                  <td align="center" width="50%">`}
+                    <Product data={this.props.data[4]} />
+                {`</td>
+                  <td align="center" width="50%">`}
+                    <Product data={this.props.data[5]} />
+                {`</td>
+                </tr>
+              </table>
+            </div>
+            <!--<![endif]-->`}
           </div>
         );
       case "7":
         return (
         	<div id="snippet">
-          {`<table align="center" width="100%" border="0" cellpadding="0" cellspacing="0">
+          {style +
+          `<table align="center" width="100%" border="0" cellpadding="0" cellspacing="0" class="hide_mobile">
             <tr>
               <td align="center" width="33.3%">`}
                 <Product data={this.props.data[0]} />
@@ -145,7 +270,7 @@ class Snippet extends Component {
               {`</td>
             </tr>
           </table>
-          <table width="100%" align="center" cellspacing="0" cellpadding="0">
+          <table width="100%" align="center" cellspacing="0" cellpadding="0" class="hide_mobile">
             <tr>
               <td width="50%" align="center">`}
                 <Product data={this.props.data[3]} />
@@ -155,7 +280,7 @@ class Snippet extends Component {
             {`</td>
             </tr>
           </table>
-          <table width="100%" align="center" cellspacing="0" cellpadding="0">
+          <table width="100%" align="center" cellspacing="0" cellpadding="0" class="hide_mobile">
             <tr>
               <td width="50%" align="center">`}
                 <Product data={this.props.data[5]} />
@@ -164,97 +289,178 @@ class Snippet extends Component {
                 <Product data={this.props.data[6]} />
             {`</td>
             </tr>
-          </table>`}
+          </table>
+          <!-- mobile view -->
+          <!--[if !mso]><!-- -->
+          <div class="show_for_mobile" align="center" style="display: none; width: 0px; height: 0px; overflow: hidden; min-height: 0px; line-height: 0px; font-size: 0px;">
+            <table align="center" width="100%" border="0" cellpadding="0" cellspacing="0">
+              <tr>
+                <td align="center" width="50%">`}
+                  <Product data={this.props.data[0]} />
+              {`</td>
+                <td align="center" width="50%">`}
+                  <Product data={this.props.data[1]} />
+              {`</td>
+              </tr>
+            </table>
+            <table align="center" width="100%" border="0" cellpadding="0" cellspacing="0">
+              <tr>
+                <td align="center" width="50%">`}
+                  <Product data={this.props.data[2]} />
+              {`</td>
+                <td align="center" width="50%">`}
+                  <Product data={this.props.data[3]} />
+              {`</td>
+              </tr>
+            </table>
+            <table align="center" width="100%" border="0" cellpadding="0" cellspacing="0">
+              <tr>
+                <td align="center" width="50%">`}
+                  <Product data={this.props.data[4]} />
+              {`</td>
+                <td align="center" width="50%">`}
+                  <Product data={this.props.data[5]} />
+              {`</td>
+              </tr>
+            </table>
+          </div>
+          <!--<![endif]-->`}
         </div>
         );
         case "8":
           return (
             <div id="snippet">
-			{`<table width="100%" align="center" cellspacing="0" cellpadding="0">
-					<tr>
-						<td width="50%" align="center">`}
-      <Product data={this.props.data[0]} />
-      {`</td>
-						<td width="50%" align="center">`}
-      <Product data={this.props.data[1]} />
-      {`</td>
-				</tr>
-			</table>
-    	<table width="100%" align="center" cellspacing="0" cellpadding="0">
-				<tr>
-					<td width="50%" align="center">`}
-  			    <Product data={this.props.data[2]} />
-      	{`</td>
-					<td width="50%" align="center">`}
-      			<Product data={this.props.data[3]} />
-      	{`</td>
-				</tr>
-			</table>
-			<table width="100%" align="center" cellspacing="0" cellpadding="0">
-				<tr>
-					<td width="50%" align="center">`}
-      			<Product data={this.props.data[4]} />
-      	{`</td>
-					<td width="50%" align="center">`}
-      			<Product data={this.props.data[5]} />
-      	{`</td>
-				</tr>
-			</table>
-    	<table width="100%" align="center" cellspacing="0" cellpadding="0">
-				<tr>
-					<td width="50%" align="center">`}
-  			    <Product data={this.props.data[6]} />
-      	{`</td>
-					<td width="50%" align="center">`}
-      			<Product data={this.props.data[7]} />
-      	{`</td>
-				</tr>
-			</table>`}
-		</div>
+            {`<table width="100%" align="center" cellspacing="0" cellpadding="0">
+                <tr>
+                  <td width="50%" align="center">`}
+            <Product data={this.props.data[0]} />
+            {`</td>
+                  <td width="50%" align="center">`}
+            <Product data={this.props.data[1]} />
+            {`</td>
+              </tr>
+            </table>
+            <table width="100%" align="center" cellspacing="0" cellpadding="0">
+              <tr>
+                <td width="50%" align="center">`}
+                  <Product data={this.props.data[2]} />
+              {`</td>
+                <td width="50%" align="center">`}
+                  <Product data={this.props.data[3]} />
+              {`</td>
+              </tr>
+            </table>
+            <table width="100%" align="center" cellspacing="0" cellpadding="0">
+              <tr>
+                <td width="50%" align="center">`}
+                  <Product data={this.props.data[4]} />
+              {`</td>
+                <td width="50%" align="center">`}
+                  <Product data={this.props.data[5]} />
+              {`</td>
+              </tr>
+            </table>
+            <table width="100%" align="center" cellspacing="0" cellpadding="0">
+              <tr>
+                <td width="50%" align="center">`}
+                  <Product data={this.props.data[6]} />
+              {`</td>
+                <td width="50%" align="center">`}
+                  <Product data={this.props.data[7]} />
+              {`</td>
+              </tr>
+            </table>`}
+          </div>
           );
       case "9":
       return (
-        		<div id="snippet">
-			{`<table align="center" width="100%" border="0" cellpadding="0" cellspacing="0">
-				<tr>
-					<td align="center" width="33.3%">`}
-      			<Product data={this.props.data[0]} />
-      	{`</td>
-					<td align="center" width="33.3%">`}
-						<Product data={this.props.data[1]} />
-				{`</td>
-					<td align="center" width="33.3%">`}
-						<Product data={this.props.data[2]} />
-					{`</td>
-				</tr>
-			</table>
-			<table align="center" width="100%" border="0" cellpadding="0" cellspacing="0">
-				<tr>
-					<td align="center" width="33.3%">`}
-      			<Product data={this.props.data[3]} />
-      	{`</td>
-					<td align="center" width="33.3%">`}
-						<Product data={this.props.data[4]} />
-				{`</td>
-					<td align="center" width="33.3%">`}
-						<Product data={this.props.data[5]} />
-					{`</td>
-				</tr>
-			</table>
-			<table align="center" width="100%" border="0" cellpadding="0" cellspacing="0">
-				<tr>
-					<td align="center" width="33.3%">`}
-      			<Product data={this.props.data[6]} />
-      	{`</td>
-					<td align="center" width="33.3%">`}
-						<Product data={this.props.data[7]} />
-				{`</td>
-					<td align="center" width="33.3%">`}
-						<Product data={this.props.data[8]} />
-					{`</td>
-				</tr>
-			</table>`}
-		</div>
+    		<div id="snippet">
+          {style +
+	    		`<table align="center" width="100%" border="0" cellpadding="0" cellspacing="0" class="hide_mobile">
+			  	<tr>
+				  	<td align="center" width="33.3%">`}
+      		  	<Product data={this.props.data[0]} />
+      	  {`</td>
+					  <td align="center" width="33.3%">`}
+						  <Product data={this.props.data[1]} />
+				  {`</td>
+					  <td align="center" width="33.3%">`}
+						  <Product data={this.props.data[2]} />
+					  {`</td>
+				  </tr>
+			  </table>
+			  <table align="center" width="100%" border="0" cellpadding="0" cellspacing="0" class="hide_mobile">
+				  <tr>
+					  <td align="center" width="33.3%">`}
+      			  <Product data={this.props.data[3]} />
+      	  {`</td>
+					  <td align="center" width="33.3%">`}
+						  <Product data={this.props.data[4]} />
+				  {`</td>
+					  <td align="center" width="33.3%">`}
+						  <Product data={this.props.data[5]} />
+					  {`</td>
+				  </tr>
+			  </table>
+			  <table align="center" width="100%" border="0" cellpadding="0" cellspacing="0" class="hide_mobile">
+				  <tr>
+					  <td align="center" width="33.3%">`}
+      			  <Product data={this.props.data[6]} />
+      	  {`</td>
+					  <td align="center" width="33.3%">`}
+						  <Product data={this.props.data[7]} />
+				  {`</td>
+					  <td align="center" width="33.3%">`}
+						  <Product data={this.props.data[8]} />
+					  {`</td>
+				  </tr>
+        </table>
+        <!-- mobile view -->
+          <!--[if !mso]><!-- -->
+          <div class="show_for_mobile" align="center" style="display: none; width: 0px; height: 0px; overflow: hidden; min-height: 0px; line-height: 0px; font-size: 0px;">
+            <table align="center" width="100%" border="0" cellpadding="0" cellspacing="0">
+              <tr>
+                <td align="center" width="50%">`}
+                  <Product data={this.props.data[0]} />
+              {`</td>
+                <td align="center" width="50%">`}
+                  <Product data={this.props.data[1]} />
+              {`</td>
+              </tr>
+            </table>
+            <table align="center" width="100%" border="0" cellpadding="0" cellspacing="0">
+              <tr>
+                <td align="center" width="50%">`}
+                  <Product data={this.props.data[2]} />
+              {`</td>
+                <td align="center" width="50%">`}
+                  <Product data={this.props.data[3]} />
+              {`</td>
+              </tr>
+            </table>
+            <table align="center" width="100%" border="0" cellpadding="0" cellspacing="0">
+              <tr>
+                <td align="center" width="50%">`}
+                  <Product data={this.props.data[4]} />
+              {`</td>
+                <td align="center" width="50%">`}
+                  <Product data={this.props.data[5]} />
+              {`</td>
+              </tr>
+            </table>
+            <table align="center" width="100%" border="0" cellpadding="0" cellspacing="0">
+              <tr>
+                <td align="center" width="50%">`}
+                  <Product data={this.props.data[6]} />
+              {`</td>
+                <td align="center" width="50%">`}
+                  <Product data={this.props.data[7]} />
+              {`</td>
+              </tr>
+            </table>
+          </div>
+          <!--<![endif]-->`}
+		  </div>
       )
     default:
   }
