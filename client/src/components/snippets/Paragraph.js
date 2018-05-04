77a132dc1ddea53b523712951b8f07b23ecb34c8
Guess/carlosallende <callende@guess.com>
2018-05-04 14:45:10 -0700

    styling

--- client/src/components/snippets/Paragraph.js
+++ client/src/components/snippets/Paragraph.js
@@ -4,7 +4,7 @@ const Paragraph = props => {
   } else {
     return `<table align="center" width="75%" border="0" cellpadding="0" cellspacing="0">
 					<tr>
-							<td align="center" class="paragraph" style="padding-top:30px; padding-bottom:10px; font-family: 'Century Gothic', Futura, 'Trebuchet MS', Arial, sans-serif; font-size: 17px; color: #000000; letter-spacing: .15em;">
+							<td align="center" class="paragraph" style="padding-top:20px; font-family: 'Century Gothic', Futura, 'Trebuchet MS', Arial, sans-serif; font-size: 15px; color: #000000; letter-spacing: .15em; line-height: 200%;">
 									${props}
 							</td>
 					</tr>
