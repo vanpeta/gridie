5e132d562a94a0ab6414367be671642d20d9b2f7
Guess/carlosallende <callende@guess.com>
2018-05-04 14:30:59 -0700

    stling

--- client/src/components/snippets/Title.js
+++ client/src/components/snippets/Title.js
@@ -1,29 +1,21 @@
 const Title = props => {
 	console.log("props in TITLE======", props);
+	let size = "40"
 	if (props.copy === "") {
 		return ""
 	} else {
 		if (props.biggerTitle) {
-			return (
-				`<table align="center" width="100%" border="0" cellpadding="0" cellspacing="0">
-						<tr>
-							<td align="center" class="gridTitle" style="padding-top:40px; padding-bottom:10px; font-family: 'Century Gothic', Futura, 'Trebuchet MS', Arial, sans-serif; font-size: 50px; color: #000000; letter-spacing: .15em; font-weight: 100; text-transform: uppercase;">
-									${props.copy}
-							</td>
-						</tr>
-				</table>`
-			)
-		} else {
-			return (
-				`<table align="center" width="100%" border="0" cellpadding="0" cellspacing="0">
-						<tr>
-							<td align="center" class="gridTitle" style="padding-bottom:40px; font-family: 'Century Gothic', Futura, 'Trebuchet MS', Arial, sans-serif; font-size: 40px; color: #000000; letter-spacing: .15em; font-weight: 100; text-transform: uppercase;">
-									${props.copy}
-							</td>
-						</tr>
-				</table>`
-			);
+			size = "50"
 		}
+		return (
+			`<table align="center" width="100%" border="0" cellpadding="0" cellspacing="0">
+					<tr>
+						<td align="center" class="gridTitle" style="padding-top:40px; padding-bottom:10px; font-family: 'Century Gothic', Futura, 'Trebuchet MS', Arial, sans-serif; font-size:`+ size +`px; color: #000000; letter-spacing: .15em; font-weight: 100; text-transform: uppercase;">
+								${props.copy}
+						</td>
+					</tr>
+			</table>`
+		)
 	}
 };
 
