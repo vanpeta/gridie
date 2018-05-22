a4ea3caaa6d47e53784f8ed9ef1b4988e2ba58f5
GUESS\carlosallende <callende@guess.com>
2018-05-22 15:28:16 -0700

    secondary setting rendering conditionally

--- client/src/components/settings/Settings.js
+++ client/src/components/settings/Settings.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 
+import { connect } from "react-redux";
+
 import InputUrl from "./InputUrl"
 import InputTitle from "./InputTitle";
 import InputParagraph from "./InputParagraph";
@@ -10,6 +12,38 @@ import ShowName from "./ShowName";
 import ShowHero from "./ShowHero";
 
 class Settings extends Component {
+  renderSecondarySettings() {
+    console.log("PROPS =>", this.props)
+    if (this.props.numberOfProducts > 0 && this.props.category) {
+      return (
+        <div className="secondarySettings">
+          <div className="row">
+            <ShowHero />
+          </div>
+          <div className="row">
+            <InputTitle />
+          </div>
+          <div className="row">
+            <InputParagraph />
+          </div>
+          <div className="row">
+            <InputCta mode="top"/>
+          </div>
+          <div className="row">
+            <InputCta  mode="bottom"/>
+          </div>
+          <div className="row">
+            <ShowName />
+          </div>
+          <div className="row">
+            <ShowPrice />
+          </div>
+        </div>
+      );
+    } else {
+      return ""
+    }
+  }
   render() {
     return <div className="container settings">
         <div className="row">
@@ -18,31 +52,16 @@ class Settings extends Component {
         <div className="row">
           <InputNumberOrProducts />
         </div>
-        <div className="row">
-          <ShowHero />
-        </div>
-        <div className="row">
-          <InputTitle />
-        </div>
-        <div className="row">
-          <InputParagraph />
-        </div>
-        <div className="row">
-          <InputCta mode="top"/>
-        </div>
-        <div className="row">
-          <InputCta  mode="bottom"/>
-        </div>
-        <div className="row">
-          <ShowName />
-        </div>
-        <div className="row">
-          <ShowPrice />
-        </div>
+        {this.renderSecondarySettings()}
       </div>;
   }
 }
 
+function mapStateToProps(state) {
+  return {
+    numberOfProducts: state.numberOfProducts,
+    category: state.selectCategory
+  };
+}
 
-
-export default Settings;
+export default connect(mapStateToProps)(Settings);
